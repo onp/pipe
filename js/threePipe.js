8d0f903202caa9a7110ea619d8aae87d783aac1e
Oskar Petersons <a.more.reliable.source@gmail.com>
2014-09-10 23:13:04 -0400

    data structure and modularizing

--- js/threePipe.js
+++ js/threePipe.js
@@ -1,3 +1,5 @@
+window.onload = function () {
+
 function getCursorPosition(e) {
     var x;
     var y;
@@ -75,23 +77,15 @@ window.addEventListener('resize', onResize, false);
 
 var scene = new THREE.Scene();
 
-var pipes = new THREE.Object3D();
-scene.add( pipes );
+PIPER.pipes = []
+var visiblePipes = new THREE.Object3D();
+scene.add( visiblePipes );
 
-var nodes = new THREE.Object3D();
-scene.add( nodes );
+PIPER.nodes = {}
+var visibleNodes = new THREE.Object3D();
+scene.add( visibleNodes );
 
 document.body.appendChild(renderer.domElement);
-
-
-var sGeom = new THREE.SphereGeometry(0.1)
-var sMat = new THREE.MeshLambertMaterial({color:0xff0000})
-
-var cGeom = new THREE.CylinderGeometry(0.1,0.1,1)
-var cTrans = new THREE.Matrix4()
-cGeom.applyMatrix(cTrans.makeTranslation(0,0.5,0))
-cGeom.applyMatrix(cTrans.makeRotationX(Math.PI/2))
-var cMat = new THREE.MeshLambertMaterial({color:0x00ff00})
 
 var axisHelper = new THREE.AxisHelper(3);
 scene.add(axisHelper);
@@ -134,7 +128,8 @@ document.addEventListener("wheel",function(e){
 
 },false)
 
-var cursorNode = {};
+var cursorNode;
+var cursorSegment;
 
 
 
@@ -144,30 +139,32 @@ var cursorNode = {};
         mode = createMode
         
         deltaBox.style.display = "none"
-        cursorNode.cursorBall =  new THREE.Mesh(sGeom,sMat)
-        scene.add(cursorNode.cursorBall)
+        cursorNode =  new PIPER.Node(new THREE.Vector3())
+        scene.add(cursorNode.makeMesh())
     };
     
     createMode.leave = function () {
         mode = undefined
-        scene.remove(cursorNode.cursorBall)
+        scene.remove(cursorNode.mesh)
     }
     
     createMode.suspend = function () {
-        scene.remove(cursorNode.cursorBall)
+        scene.remove(cursorNode.mesh)
     }
     
     createMode.resume = function () {
-        scene.add(cursorNode.cursorBall)
+        scene.add(cursorNode.mesh)
     }
     
     createMode.onClick = function (e) {
-        //var newNode = new THREE.Mesh(sGeom,sMat)
-        var newNode = new Pipe.Node(cursorNode.cursorBall.position)
-        newNode.position.copy()
-        nodes.add(newNode)
+    
+        var newNode = new PIPER.Node(cursorNode.mesh.position.clone())
+
+        PIPER.nodes[newNode.uuid] = newNode
+        
+        visibleNodes.add(newNode.makeMesh())
        
-        drawMode.enter()
+        drawMode.enter(newNode)
     }
 
     createMode.onFrame = function (e) {
@@ -180,9 +177,9 @@ var cursorNode = {};
         
         var iPoint = raycaster.ray.intersectPlane(iPlane)
         
-        var intersects = raycaster.intersectObjects(pipes.children);
+        var intersects = raycaster.intersectObjects(visiblePipes.children);
         
-        pipes.children.forEach(
+        visiblePipes.children.forEach(
             function ( pipe ) {
                 pipe.material.color.setHex(0x00ff00);
             }
@@ -192,7 +189,7 @@ var cursorNode = {};
             intersects[0].object.material.color.setHex(0x0000ff);
         };
         
-        cursorNode.cursorBall.position.copy(iPoint)
+        cursorNode.mesh.position.copy(iPoint)
         
         updateCoords(iPoint)
     };
@@ -205,36 +202,44 @@ var cursorNode = {};
 // Define "draw mode" behaviour.
 (function (drawMode, undefined) {
 
-    var basisPoint
+    var sourceNode // basisPoint
 
-    drawMode.enter = function(){
+    drawMode.enter = function(currNode){
         mode = drawMode
         
-        cursorNode.cylinder = new THREE.Mesh(cGeom,cMat.clone())
-        cursorNode.cylinder.position.copy(cursorNode.cursorBall.position)
-        pipes.add(cursorNode.cylinder)
+        sourceNode = currNode
         
-        basisPoint = cursorNode.cursorBall.position.clone()
+        cursorSegment = new PIPER.Segment(sourceNode)
+        scene.add(cursorSegment.makeMesh())
         
         deltaBox.style.display = "block"
     }
     
     drawMode.suspend = function (){
-         pipes.remove(cursorNode.cylinder)
-         scene.remove(cursorNode.cursorBall)
+         scene.remove(cursorNode.cylinder)
+         scene.remove(cursorNode.mesh)
     }
     
     drawMode.resume = function () {
-         pipes.add(cursorNode.cylinder)
-         scene.add(cursorNode.cursorBall)
+         scene.add(cursorNode.cylinder)
+         scene.add(cursorNode.mesh)
     }
 
     drawMode.onClick = function (e) {
-        var newNode = new THREE.Mesh(sGeom, sMat)
-        newNode.position.copy(cursorNode.cursorBall.position)
-        nodes.add(newNode)
+    
+        var newNode = new PIPER.Node(cursorNode.mesh.position.clone())
+
+        PIPER.nodes[newNode.uuid] = newNode
         
-        drawMode.enter()
+        visibleNodes.add(newNode.makeMesh())
+
+        scene.remove(cursorNode.cylinder)
+        visiblePipes.add(cursorNode.cylinder)
+        
+        newPipe = new PIPER.Segment(sourceNode,newNode)
+        PIPER.pipes.push(newPipe)
+        
+        drawMode.enter(newNode)
     }
     
     drawMode.onFrame = function (e) {
@@ -243,37 +248,37 @@ var cursorNode = {};
         var projector = new THREE.Projector();
         var raycaster = projector.pickingRay(mouseVector.clone(),orthoCamera)
         
-        var x0 = new THREE.Vector3(-500,0,0).add(basisPoint)
-        var x1 = new THREE.Vector3(500,0,0).add(basisPoint)
+        var x0 = new THREE.Vector3(-500,0,0).add(sourceNode.position)
+        var x1 = new THREE.Vector3(500,0,0).add(sourceNode.position)
         var xPt = new THREE.Vector3()
         var xd = raycaster.ray.distanceSqToSegment(x0,x1,null,xPt)
         
-        var y0 = new THREE.Vector3(0,-500,0).add(basisPoint)
-        var y1 = new THREE.Vector3(0,500,0).add(basisPoint)
+        var y0 = new THREE.Vector3(0,-500,0).add(sourceNode.position)
+        var y1 = new THREE.Vector3(0,500,0).add(sourceNode.position)
         var yPt = new THREE.Vector3()
         var yd = raycaster.ray.distanceSqToSegment(y0,y1,null,yPt)
         
-        var z0 = new THREE.Vector3(0,0,-500).add(basisPoint)
-        var z1 = new THREE.Vector3(0,0,500).add(basisPoint)
+        var z0 = new THREE.Vector3(0,0,-500).add(sourceNode.position)
+        var z1 = new THREE.Vector3(0,0,500).add(sourceNode.position)
         var zPt = new THREE.Vector3()
         var zd = raycaster.ray.distanceSqToSegment(z0,z1,null,zPt)
         
         var pt = (xd < yd) ? ((xd < zd)? xPt : zPt) : ((yd < zd) ? yPt : zPt)
         
 
-        cursorNode.cursorBall.position.copy(pt)
+        cursorNode.mesh.position.copy(pt)
 
-        cursorNode.cylinder.scale.set(1,1,pt.clone().sub(basisPoint).length())
-        cursorNode.cylinder.lookAt(pt)
+        cursorSegment.mesh.scale.set(1,1,pt.clone().sub(sourceNode.position).length())
+        cursorSegment.mesh.lookAt(pt)
         
         updateCoords(pt)
-        updateDelta(pt.clone().sub(basisPoint))
+        updateDelta(pt.clone().sub(sourceNode.position))
     }
     
     drawMode.onKeyDown = function(e){
         if (e.keyCode ==27){
-            scene.remove(cursorNode.cursorBall)
-            pipes.remove(cursorNode.cylinder)
+            scene.remove(cursorNode.mesh)
+            scene.remove(cursorNode.cylinder)
             
             createMode.enter()
             
@@ -347,4 +352,4 @@ renderer.render(scene,orthoCamera);
 
 render()
     
-    
+}
