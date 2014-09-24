d30b5c628672ec135391a9ccc36277bbc450e942
Oskar Petersons <a.more.reliable.source@gmail.com>
2014-09-24 19:05:41 -0400

    more advanced cursor positioning

--- js/threePipe.js
+++ js/threePipe.js
@@ -23,7 +23,21 @@ function updateDelta(vec){
     document.getElementById("z-delta").value = vec.z.toFixed(3)
     document.getElementById("x-delta").value = vec.x.toFixed(3)
     document.getElementById("y-delta").value = vec.y.toFixed(3)
+    document.getElementById("l-delta").value = vec.length().toFixed(3)
 }
+
+var positionSpecs = {
+    x : undefined,
+    y : undefined,
+    z : 0,
+    xd : undefined,
+    yd : undefined,
+    zd : undefined,
+    l  : undefined
+};
+
+
+    
 
 
 var mouseState = {x:0,y:0,right:false,left:false}
@@ -133,7 +147,146 @@ var cursorSegment;
 
 
 
-// 
+function blarrg (n0,castRay,dx,dy,dz,l ) {
+// n0: origin node [Vector3]
+// castRay: mouse ray [RayCaster]
+// dx: locks the returned point to x = n0.x + dx
+// dy: locks the returned point to y = n0.y + dy
+// dz: locks the returned point to z = n0.z + dz
+// l:  forces the returned point to be distance l from n0
+
+	var a,b,norm;
+
+	if ( l !== undefined ){
+
+		var closestPoint = n0.clone().add( new THREE.Vector3( l, 0, 0 ) )
+
+		var minDist = castRay.distanceToPoint(closestPoint)
+
+		var axisVectors = [	new THREE.Vector3( -l, 0, 0 ),
+					new THREE.Vector3( 0,  l, 0 ),
+					new THREE.Vector3( 0, -l, 0 ),
+					new THREE.Vector3( 0, 0,  l ),
+					new THREE.Vector3( 0, 0, -l )
+				  ];
+		var point2, dist2
+
+		for ( var i = 0; i < axisVectors.length; i++ ) {
+
+			point2 = n0.clone().add( axisVector[i] )
+
+			dist2 = castRay.distanceToPoint( point2 )
+
+			if ( dist2 < minDist) {
+
+				closestPoint = point2;
+
+				minDist = dist2;
+
+			};
+		};
+
+		return closestPoint;
+
+	}
+
+	if ( dx !== undefined ) {	
+		
+		dx = new THREE.Vector3(dx,0,0);
+
+		a = dx;
+        norm = new THREE.Vector3(1,0,0);
+
+	};
+
+
+	if ( dy !== undefined ) {	
+		
+		dy = new THREE.Vector3(0,dy,0);
+
+		if ( a === undefined ) {
+
+			a = dy;
+            norm = new THREE.Vector3(0,1,0);
+
+		} else {
+
+			b = dy;
+
+		};
+
+	};
+
+	if ( dz !== undefined ) {	
+		
+		dz = new THREE.Vector3(0,0,dz);
+
+		if ( a === undefined ) {
+
+			a = dz;
+            norm = new THREE.Vector3(0,0,1);
+
+		} else if ( b === undefined ) {
+
+			b = dz;
+
+		} else {
+
+			return n0.add(dx).add(dy).add(dz);
+
+		};
+
+	};
+
+	if ( a === undefined ) {
+        var x0 = new THREE.Vector3(-500,0,0).add(n0)
+        var x1 = new THREE.Vector3(500,0,0).add(n0)
+        var xPt = new THREE.Vector3()
+        var xd = castRay.ray.distanceSqToSegment(x0,x1,null,xPt)
+        
+        var y0 = new THREE.Vector3(0,-500,0).add(n0)
+        var y1 = new THREE.Vector3(0,500,0).add(n0)
+        var yPt = new THREE.Vector3()
+        var yd = castRay.ray.distanceSqToSegment(y0,y1,null,yPt)
+        
+        var z0 = new THREE.Vector3(0,0,-500).add(n0)
+        var z1 = new THREE.Vector3(0,0,500).add(n0)
+        var zPt = new THREE.Vector3()
+        var zd = castRay.ray.distanceSqToSegment(z0,z1,null,zPt)
+        
+        var pt = (xd < yd) ? ((xd < zd)? xPt : zPt) : ((yd < zd) ? yPt : zPt)
+        
+        return pt
+
+	} else if ( b === undefined ) {
+
+		//fix to plane on n0, with normal of a
+
+		var targetPlane = new THREE.Plane()
+
+		targetPlane.setFromNormalAndCoplanarPoint(norm,n0)
+        
+        var pt = castRay.ray.intersectPlane(targetPlane)
+        
+        return pt || new THREE.Vector3()
+
+	} else {
+
+		//fix to line on n0, perpendicular to a and b
+
+		var c = new THREE.Vector3();
+
+		c.crossVectors(a,b).normalize().multiplyscalar(500);
+
+		var closePoint = new THREE.Vector3();
+
+		castRay.distanceSqToSegment(n0.clone.sub(c),n0.clone.add(c),undefined,closePoint1);
+
+		return closePoint;
+
+	}
+
+}
 
 
 
@@ -188,9 +341,13 @@ var cursorSegment;
         var projector = new THREE.Projector();
         var raycaster = projector.pickingRay(mouseVector.clone(),orthoCamera)
         
-        var iPlane = new THREE.Plane(new THREE.Vector3(0,0,1),0)
+        var blah = new THREE.Vector3()
         
-        var iPoint = raycaster.ray.intersectPlane(iPlane)
+        var iPoint = blarrg(blah,raycaster,positionSpecs.x,positionSpecs.y,positionSpecs.z)
+        
+        //var iPlane = new THREE.Plane(new THREE.Vector3(0,0,1),0)
+        
+        //var iPoint = raycaster.ray.intersectPlane(iPlane)
         
         var intersects = raycaster.intersectObjects(visiblePipes.children);
         
@@ -203,6 +360,7 @@ var cursorSegment;
         if (intersects.length > 0){
             intersects[0].object.material.color.setHex(0x0000ff);
             createMode.underMouse = intersects[0].object.userData.owner
+            
         } else {
             createMode.underMouse = null;
         };
@@ -267,23 +425,9 @@ var cursorSegment;
         var projector = new THREE.Projector();
         var raycaster = projector.pickingRay(mouseVector.clone(),orthoCamera)
         
-        var x0 = new THREE.Vector3(-500,0,0).add(sourceNode.position)
-        var x1 = new THREE.Vector3(500,0,0).add(sourceNode.position)
-        var xPt = new THREE.Vector3()
-        var xd = raycaster.ray.distanceSqToSegment(x0,x1,null,xPt)
+        var iPoint = blarrg(sourceNode.position.clone(),raycaster,positionSpecs.xd,positionSpecs.yd,positionSpecs.zd,positionSpecs.l)
         
-        var y0 = new THREE.Vector3(0,-500,0).add(sourceNode.position)
-        var y1 = new THREE.Vector3(0,500,0).add(sourceNode.position)
-        var yPt = new THREE.Vector3()
-        var yd = raycaster.ray.distanceSqToSegment(y0,y1,null,yPt)
-        
-        var z0 = new THREE.Vector3(0,0,-500).add(sourceNode.position)
-        var z1 = new THREE.Vector3(0,0,500).add(sourceNode.position)
-        var zPt = new THREE.Vector3()
-        var zd = raycaster.ray.distanceSqToSegment(z0,z1,null,zPt)
-        
-        var pt = (xd < yd) ? ((xd < zd)? xPt : zPt) : ((yd < zd) ? yPt : zPt)
-        
+        pt = iPoint
 
         cursorNode.mesh.position.copy(pt)
 
